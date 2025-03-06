111d560614cf7069e60c732fba46bab2d9bb5509
danielkoo <koo.donghyun99@gmail.com>
2025-03-06 19:49:31 +0900

    feat: open and close the modal
    This commit adds the ability to open and close the dropdown menu.
    It adds a state to track whether it is open or not and shows the appropriate caret icon and only shows the dropdown menu's content when open.

--- src/components/dropdown/index.tsx
+++ src/components/dropdown/index.tsx
@@ -2,7 +2,9 @@
  * @fileoverview a custom dropdown component
  * allows single and multi select
  */
-import React from "react";
+import React, {
+    useState
+} from "react";
 import '../../App.css';
 import CaretDown from '../../assets/icons/caret-down.svg';
 import CaretUp from '../../assets/icons/caret-up.svg';
@@ -12,58 +14,64 @@ interface DropdownProps {
 }
 
 
-const Dropdown : React.FC<DropdownProps> = ({
+const Dropdown: React.FC<DropdownProps> = ({
     children,
 }) => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+
     return (
         <>
             <div className="dropdown">
                 {/*     Placeholder / Selected Text Field    */}
-                <div className="dropdown-button">
+                <div className="dropdown-button" onClick={() => setIsOpen(!isOpen)}>
                     Choose One
-                    {/*<img src={CaretDown} alt="Caret Down"/>*/}
-                    <img src={CaretUp} alt="Caret Up"/>
-                </div>
+                    {isOpen
+                        ? <img src={CaretUp} alt="Caret Up"/>
+                        : <img src={CaretDown} alt="Caret Down"/>
+                }
+            </div>
 
                 {/*     Dropdown Menu Items     */}
-                <div className="dropdown-content">
-                    <div className="dropdown-item">
-                        option A
+                {isOpen &&
+                    <div className="dropdown-content">
+                        <div className="dropdown-item">
+                            option A
+                        </div>
+                        <div className="dropdown-item">
+                            option B
+                        </div>
+                        <div className="dropdown-item">
+                            option C
+                        </div>
+                        <div className="dropdown-item">
+                            option D
+                        </div>
+                        <div className="dropdown-item">
+                            option E
+                        </div>
+                        <div className="dropdown-item">
+                            option F
+                        </div>
+                        <div className="dropdown-item">
+                            option G
+                        </div>
+                        <div className="dropdown-item">
+                            option H
+                        </div>
+                        <div className="dropdown-item">
+                            option I
+                        </div>
+                        <div className="dropdown-item">
+                            option J
+                        </div>
+                        <div className="dropdown-item">
+                            option K
+                        </div>
+                        <div className="dropdown-item">
+                            option L
+                        </div>
                     </div>
-                    <div className="dropdown-item">
-                        option B
-                    </div>
-                    <div className="dropdown-item">
-                        option C
-                    </div>
-                    <div className="dropdown-item">
-                        option D
-                    </div>
-                    <div className="dropdown-item">
-                        option E
-                    </div>
-                    <div className="dropdown-item">
-                        option F
-                    </div>
-                    <div className="dropdown-item">
-                        option G
-                    </div>
-                    <div className="dropdown-item">
-                        option H
-                    </div>
-                    <div className="dropdown-item">
-                        option I
-                    </div>
-                    <div className="dropdown-item">
-                        option J
-                    </div>
-                    <div className="dropdown-item">
-                        option K
-                    </div>
-                    <div className="dropdown-item">
-                        option L
-                    </div>
-                </div>
+                }
             </div>
         </>
     )
