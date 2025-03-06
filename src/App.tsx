6d10bf52f525278125da40074dc5855e0c99e111
danielkoo <koo.donghyun99@gmail.com>
2025-03-06 20:21:54 +0900

    refactor: change dropdown to named export

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Dropdown from './components/dropdown'
+import { Dropdown } from './components/dropdown'
 
 function App() {
   return (
---
+++ src/components/dropdown/Dropdown.tsx
@@ -0,0 +1,86 @@
+/**
+ * @fileoverview a custom dropdown component
+ * allows single and multi select
+ */
+import React, {
+    useState
+} from 'react';
+import '../../App.css';
+import CaretDown from '../../assets/icons/caret-down.svg';
+import CaretUp from '../../assets/icons/caret-up.svg';
+
+interface DropdownProps {
+    children?: React.ReactNode;
+}
+
+
+export const Dropdown: React.FC<DropdownProps> = ({
+    children,
+}) => {
+    const [isOpen, setIsOpen] = useState<boolean>(false);
+    const [selected, setSelected] = useState<string[]>([]);
+
+
+    return (
+        <>
+            <div className="dropdown">
+                {/*     Text Field    */}
+                <div className="dropdown-button" onClick={() => setIsOpen(!isOpen)}>
+                    {/*     Placeholder if empty    */}
+                    {selected.length === 0
+                        ? "Choose One"
+                        : selected.toString()}
+                    {isOpen
+                        ? <img src={CaretUp} alt="Caret Up"/>
+                        : <img src={CaretDown} alt="Caret Down"/>}
+                </div>
+
+                {/*     Dropdown Menu Items     */}
+                {isOpen &&
+                    <div className="dropdown-content">
+                        <div className="dropdown-item">
+                            option A
+                        </div>
+                        <div className="dropdown-item">
+                            option B
+                        </div>
+                        <div className="dropdown-item">
+                            option C
+                        </div>
+                        <div className="dropdown-item">
+                            A super duper long option that should not fit
+                            if it is not handled properly
+                        </div>
+                        <div className="dropdown-item">
+                            option D
+                        </div>
+                        <div className="dropdown-item">
+                            option E
+                        </div>
+                        <div className="dropdown-item">
+                            option F
+                        </div>
+                        <div className="dropdown-item">
+                            option G
+                        </div>
+                        <div className="dropdown-item">
+                            option H
+                        </div>
+                        <div className="dropdown-item">
+                            option I
+                        </div>
+                        <div className="dropdown-item">
+                            option J
+                        </div>
+                        <div className="dropdown-item">
+                            option K
+                        </div>
+                        <div className="dropdown-item">
+                            option L
+                        </div>
+                    </div>
+                }
+            </div>
+        </>
+    )
+}
